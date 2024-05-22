9b52fbe2d018cc5c2dabaace0bbbfb709583e7fb
pawansarap <pawansarap519@gmail.com>
2024-05-22 21:05:42 +0530

    modify the code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import StartRating from './StartRating';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={10}/>
-    <StartRating />
+    <StarRating maxRating={10}/>
+    <StarRating />
   </React.StrictMode>
 );
 
