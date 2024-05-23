9a18d5d27a0f6bb673edfe2650e57db86a7eb952
pawansarap <pawansarap519@gmail.com>
2024-05-23 23:13:47 +0530

    Updated the StarRating Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test(){
+  const [movieRating, setMovieRating] = useState(0)
+  return(
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10}/>
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={45}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test/>
+    
   </React.StrictMode>
 );
-
